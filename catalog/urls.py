737c480b9f783579a405cc576eb1a559b756e4b7
koipish <koipish@gmail.com>
2024-04-08 21:27:51 +0300

    added includes

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 urlpatterns = [
     path('contacts/', index_contacts, name="index_contacts"),
-    path('', index_home,name="index_home"),
+    path('', index_home, name="index_home"),
     path('<int:pk>/catalog/', product, name="product")
 ]
