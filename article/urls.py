a3183d8b0e84ad18e17acd4b25538e5df295e63c
koipish <koipish@gmail.com>
2024-05-15 23:25:13 +0300

    full site cache

--- article/urls.py
+++ article/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
+
 from article.views import ArticleCreateView, ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, \
     toggle_activity
 from article.apps import ArticleConfig
 app_name = ArticleConfig.name
 urlpatterns = [
-    path('articles/create/', ArticleCreateView.as_view(), name="create_article"),
-    path('articles/update/<int:pk>/', ArticleUpdateView.as_view(), name="update_article"),
+    path('articles/create/', never_cache(ArticleCreateView.as_view()), name="create_article"),
+    path('articles/update/<int:pk>/', never_cache(ArticleUpdateView.as_view()), name="update_article"),
     path('articles/', ArticleListView.as_view(), name="articles"),
     path('articles/<slug:slug>/', ArticleDetailView.as_view(), name="article_detail"),
     path('articles/delete/<int:pk>/', ArticleDeleteView.as_view(), name="delete_article"),
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.views import (index_contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView,
                            ProductDeleteView, CategoryListView)
@@ -9,8 +9,8 @@ urlpatterns = [
     path('contacts/', index_contacts, name="index_contacts"),
     path('', ProductListView.as_view(), name="index_home"),
     path('<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name="product"),
-    path('create/', ProductCreateView.as_view(), name="create_product"),
-    path('update/<int:pk>', ProductUpdateView.as_view(), name="update_product"),
+    path('create/', never_cache(ProductCreateView.as_view()), name="create_product"),
+    path('update/<int:pk>', never_cache(ProductUpdateView.as_view()), name="update_product"),
     path('delete/<int:pk>', ProductDeleteView.as_view(), name="delete_product"),
     path('category_list/', CategoryListView.as_view(), name="category_list"),
 
